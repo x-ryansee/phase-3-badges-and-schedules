7b11c5ae71bfd68df0115d6b3e2579911dc44f9a
Ryan See <ryanjosephsee@gmail.com>
2022-12-15 08:49:03 -0800

    commit tree 21e19cf8eb9fe5d05471aef597ea330b0b8bfb80

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker (name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+    attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+   end
+end
+
+def assign_rooms (attendees)
+    attendees.map.with_index(1) do |attendee, room_num|
+        "Hello, #{attendee}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def printer (attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+    assign_rooms(attendees).each do |assignment|
+        puts assignment
+    end
+end
